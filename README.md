1ea5b00178ee3707cd5ca5bd6407b196de008fa7
Will Russell <will@mlh.io>
2023-01-23 12:31:12 +0000

    Make sure commits are within batch dates

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Project Metrics
 
 A script to automatically pull the data from GitHub about Fellowship Projects and store them in a database.
+
+## Cron setup
+
+```
+crontab -e
+```
+
+Run daily at 8am GMT
+```
+0 8 * * * python3 script.py
+```
--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import json
 from pprint import pprint
 import requests
 import time
+import datetime
+import pytz
 
 load_dotenv()
 
@@ -29,53 +31,68 @@ BASE_URL = "https://api.github.com/search/"
 COMMITS_URL = "commits?q=author:"
 ISSUES_URL = "issues?q=author:"
 
-BATCH_START = "2022-09-19T00:00:00Z" #22.FAL Start
+BATCH_START = datetime.datetime(2022, 9, 19)
+BATCH_END = datetime.datetime(2022, 12, 10)
+GITHUB_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+GITHUB_COMMIT_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
+utc = pytz.utc
 
 def make_request(request_type, user):
-    r = requests.get(BASE_URL + request_type + user + "&per_page=100")
+    r = None
+    if request_type == ISSUES_URL:
+        r = requests.get(BASE_URL + request_type + user + "&per_page=100")
+    elif request_type == COMMITS_URL:
+        r = requests.get(BASE_URL + request_type + user + "&per_page=100&&sort=author-date")
+
     return r.json()    
 
 def find_issues_prs(response, projects, fellow):
     for item in response["items"]:
         url = '/'.join(item['html_url'].split('/')[:5])
-        #if item['created_at'] # Figure out data
-
-        if url in projects and "pull_request" in item: # if it's a PR
-            fellows[fellow]["prs"].append({
-                "title": item['title'],
-                "url": item['html_url'],
-                "created_at": item['created_at'],
-                "closed_at": item['closed_at'],
-                "merged_at": item['pull_request']['merged_at']
-            })
-        elif url in projects and "pull_request" not in item: #if it's a PR
-            fellows[fellow]["issues"].append({
-                "title": item['title'],
-                "url": item['html_url'],
-                "created_at": item['created_at'],
-                "closed_at": item['closed_at'],
-            })
+        
+        # Check dates are within Batch Dates
+        if datetime.datetime.strptime(item['created_at'], GITHUB_DATE_FORMAT) >= BATCH_START and datetime.datetime.strptime(item['created_at'], GITHUB_DATE_FORMAT) <= BATCH_END:
+            # Check PR is in the project
+            if url in projects and "pull_request" in item: # if it's a PR
+                fellows[fellow]["prs"].append({
+                    "title": item['title'],
+                    "url": item['html_url'],
+                    "created_at": item['created_at'],
+                    "closed_at": item['closed_at'],
+                    "merged_at": item['pull_request']['merged_at']
+                })
+            # Check Issue is in the project
+            elif url in projects and "pull_request" not in item: #if it's an Issue
+                fellows[fellow]["issues"].append({
+                    "title": item['title'],
+                    "url": item['html_url'],
+                    "created_at": item['created_at'],
+                    "closed_at": item['closed_at'],
+                })
 
 # this needs to identify forks
 def find_commits(response, projects, fellow):
     for item in response['items']:
         url = item['repository']['html_url']
-        if url in projects:
-            fellows[fellow]['commits'].append({
-                "title": item['title'],
-                "url": item['html_url'],
-                "created_at": item['commit']['author']['date']
-            })
+
+        if (datetime.datetime.strptime(item['commit']['author']['date'], GITHUB_COMMIT_DATE_FORMAT)).replace(tzinfo=utc) >= BATCH_START.replace(tzinfo=utc) and datetime.datetime.strptime(item['commit']['author']['date'], GITHUB_COMMIT_DATE_FORMAT).replace(tzinfo=utc) <= BATCH_END.replace(tzinfo=utc):
+            if url in projects:
+                fellows[fellow]['commits'].append({
+                    "commit_message": item['commit']['message'],
+                    "url": item['repository']['html_url'],
+                    "created_at": item['commit']['author']['date']
+                })
 
 
-# issues/prs
+# Get info per fellow
 for fellow in fellows:
-    response = make_request(ISSUES_URL, fellows[fellow]['github_username'])
     fellow_projects = projects[fellows[fellow]['project']]
-    find_issues_prs(response, fellow_projects, fellow)
     
-    #response = make_request(COMMITS_URL, fellows[fellow]['github_username'])
-    #find_commits(response, fellow_projects, fellow)
+    issues_response = make_request(ISSUES_URL, fellows[fellow]['github_username'])
+    find_issues_prs(issues_response, fellow_projects, fellow)
+    
+    commits_response = make_request(COMMITS_URL, fellows[fellow]['github_username'])
+    find_commits(commits_response, fellow_projects, fellow)
 
     pprint(fellows[fellow])
     break
