397e0dba407dc9bedb042c70554790933fecabca
Will Russell <will@mlh.io>
2023-03-08 17:35:09 +0000

    Fix merged date for GitLab repos

--- script.py
+++ script.py
@@ -186,10 +186,10 @@ def check_no_duplicates(url, closed_date="Null", merged_date="Null"):
         if item[0].strip() == url:
             if closed_date != "Null" and closed_date != None:
                 activities_data_sh.update_acell(f"K{row + 2}", closed_date)                
-                if merged_date != "Null" and merged_date != None:
-                    activities_data_sh.update_acell(f"L{row + 2}", merged_date)
-                    get_pr_changed_lines(url, row)
-                time.sleep(0.1)
+            if merged_date != "Null" and merged_date != None:
+                activities_data_sh.update_acell(f"L{row + 2}", merged_date)
+                get_pr_changed_lines(url, row)
+            time.sleep(0.1)
 
             return False
     return True
