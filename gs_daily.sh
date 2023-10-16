423ad4e0ef2a9f3347207d5e92cd0a362aba630b
Will Russell <will@mlh.io>
2023-10-16 18:32:22 +0100

    Change file to run

--- gs_daily.sh
+++ gs_daily.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 source .venv/bin/activate
 pip install -r requirements.txt
-python3 script.py
+python3 git_metrics.py
