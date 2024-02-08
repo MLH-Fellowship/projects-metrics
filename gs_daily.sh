909c11e6764b18726d58cfd49331f3aae7786a9d
Will Russell <will@mlh.io>
2024-02-08 11:51:48 +0000

    Add gs_credentials to pipeline

--- gs_daily.sh
+++ gs_daily.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+echo -n "$GC_SA" | base64 -d > gs_credentials.json
 source .venv/bin/activate
 pip install -r requirements.txt
 python3 git_metrics.py
